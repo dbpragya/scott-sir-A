8fa943597fc22e1310409b803572bd7da436d634
dbanmol <dbanmol.j@gmail.com>
2025-05-15 16:14:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = require("./src/app");
 
 const PORT = process.env.PORT || 5000;
 
+app.use('/', (req, res) => {
+  res.send("Welcome to the server");
+});
+
 
 app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
