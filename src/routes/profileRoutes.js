1cf745b8eb60c02d7adee8cc92a66362d422670d
dbanmol <dbanmol.j@gmail.com>
2025-05-19 17:44:19 +0530

    Update profileRoutes.js

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getProfile, updateProfile, getTotalEvents, changePassword, logout, updateAllNotifications, updateChatNotifications } = require("../controllers/profileController");
+const { getProfile, updateProfile, getTotalEvents, changePassword, logout, updateAllNotifications, updateChatNotifications, getPlan, purchasePlan } = require("../controllers/profileController");
 const authenticateUser = require("../middleware/authmiddleware")
 
 router.get("/profile", authenticateUser, getProfile);
-router.put('/update-profile', authenticateUser, updateProfile); 
+router.put('/update-profile', authenticateUser, updateProfile);
 router.get('/total-events', authenticateUser, getTotalEvents);
 router.put('/change-password', authenticateUser, changePassword);
 router.post('/logout', authenticateUser, logout);
 router.put('/all-notifications', authenticateUser, updateAllNotifications);
-router.put('/chat-notifications',  authenticateUser, updateChatNotifications);
+router.put('/chat-notifications', authenticateUser, updateChatNotifications);
+
+// for premium plan
+router.get('/premium-plan', authenticateUser, getPlan);
+router.post('/purchase/:planId', authenticateUser, purchasePlan);
+
+
 
 module.exports = router;
