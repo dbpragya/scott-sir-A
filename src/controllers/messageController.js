7d106a22b9c92e1707a5e9abc7f6367cb20ca9fe
dbanmol <dbanmol.j@gmail.com>
2025-05-23 15:03:37 +0530

    Update messageController.js

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,5 +1,6 @@
 const Message = require("../models/Message");
 const Group = require("../models/Group");
+const Event = require("../models/Event");
 
 exports.getGroupMessages = async (req, res) => {
   try {
@@ -10,9 +11,21 @@ exports.getGroupMessages = async (req, res) => {
     if (!group) {
       return res.status(404).json({ status: false, message: "Group not found" });
     }
-    if (!group.members.some(m => m.toString() === userId)) {
+
+    const isMember = group.members.some(m => m.toString() === userId);
+
+    let isEventCreator = false;
+    if (group.eventId) {
+      const event = await Event.findById(group.eventId);
+      if (event && event.createdBy.toString() === userId) {
+        isEventCreator = true;
+      }
+    }
+
+    if (!isMember && !isEventCreator) {
       return res.status(403).json({ status: false, message: "Access denied" });
     }
+
     const messages = await Message.find({ groupId })
       .sort({ sentAt: 1 })
       .populate("sender", "name avatar");
