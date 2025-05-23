5bc685c3b69650676f27d228f948561abd9985c2
dbanmol <dbanmol.j@gmail.com>
2025-05-23 15:05:00 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const { login,signup,verifyOtp,resendOtp, createPassword, uploadProfilePicture} = require('../controllers/authController');
-const {signupValidation,verifyOtpValidation } = require("../validators/validation");
+const { signupValidationRules, verifyOtpValidationRules, createPasswordValidationRules, resendOtpValidationRules, loginValidationRules } = require("../validators/validation");
 const router = express.Router();
 const authenticateUser = require('../middleware/authmiddleware');
 const upload = require('../middleware/uploadMiddleware'); 
 
 router.get("/test", (req, res) => {res.send("Auth route working!");});
-router.post("/login", login);
-router.post("/signup", signupValidation , signup);
-router.post("/verify-otp", verifyOtpValidation, verifyOtp);
-router.post("/resend-otp", resendOtp);
-router.post("/create-password", signupValidation , createPassword);
+router.post("/login", loginValidationRules, login);
+router.post("/signup", signupValidationRules, signup);
+router.post("/verify-otp", verifyOtpValidationRules, verifyOtp);
+router.post("/resend-otp", resendOtpValidationRules, resendOtp);
+router.post("/create-password", createPasswordValidationRules, createPassword);
 router.post('/upload-profile-picture', authenticateUser, upload, uploadProfilePicture);
 
 module.exports = router;
