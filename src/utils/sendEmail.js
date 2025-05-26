e886e96e1084f4e41482d56012dd7c520242882c
dbanmol <dbanmol.j@gmail.com>
2025-05-26 10:59:38 +0530

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 
 async function sendEmail(to, subject, text) {
   const mailOptions = {
-    from: `"Your App Name" <${EMAIL}>`,
+    from: `"MakeItHappen" <${EMAIL}>`,
     to,
     subject,
     text,
